7869a84fa244eafc458fce6b6640f1e4cd73e389
siyeon9302 <siyeon9302@gmail.com>
2025-05-06 19:52:45 +0900

    feat: 2주차 과제 파일 생성 완료

---
+++ practice/Siyeon/week2/src/data.js
@@ -0,0 +1,23 @@
+export const people = [
+    {
+      id: 1,
+      name: 'Marie Curie',
+      field: 'Chemistry',
+      achievement: 'Radioactivity Research',
+      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/6/69/Marie_Curie_c1920.jpg',
+    },
+    {
+      id: 2,
+      name: 'Alan Turing',
+      field: 'Mathematics',
+      achievement: 'Foundations of Computer Science',
+      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/a/a1/Alan_Turing_Aged_16.jpg',
+    },
+    {
+      id: 3,
+      name: 'Ada Lovelace',
+      field: 'Computing',
+      achievement: 'First Computer Programmer',
+      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/0/0f/Ada_Lovelace_portrait.jpg',
+    },
+  ];
